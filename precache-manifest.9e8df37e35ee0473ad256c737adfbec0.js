c705b252b30a0b5b804f6df54e3a670dc4d93d38
Alexey <alexey-032@ya.ru>
2020-02-07 15:23:15 +0300

    Updates

---
+++ precache-manifest.9e8df37e35ee0473ad256c737adfbec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c5496a891f9989c76f144b8e1c3b13",
+    "url": "/react-future-test/index.html"
+  },
+  {
+    "revision": "0af46b0bfc588b8b86a4",
+    "url": "/react-future-test/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a7846f2460dccfca1e5d",
+    "url": "/react-future-test/static/css/main.9838c551.chunk.css"
+  },
+  {
+    "revision": "0af46b0bfc588b8b86a4",
+    "url": "/react-future-test/static/js/2.f8c5e791.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-future-test/static/js/2.f8c5e791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7846f2460dccfca1e5d",
+    "url": "/react-future-test/static/js/main.8df08d4d.chunk.js"
+  },
+  {
+    "revision": "f4f87d0fd9ab6b887c96",
+    "url": "/react-future-test/static/js/runtime-main.29161b34.js"
+  }
+]);
